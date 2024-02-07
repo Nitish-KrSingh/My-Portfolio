96e845ca2e9292a5f0bc53d40f1f87f30d2d8c82
nitish <nitishbcps@gmail.com>
2024-02-07 11:54:15 +0530

    timeline details

---
+++ src/Pages/Home/Timeline/TimelineElement.js
@@ -0,0 +1,47 @@
+let timelineElement = [
+    {
+        id : 1,
+        title1 : "Manipal Institute of Technology",
+        title2 : "Master of Computer Application",
+        location : "Manipal",
+        descrption : "CGPA: 7.06*",
+        date : "Sep 2022 – Present",
+        icon : "school",
+    },
+    {
+        id : 2,
+        title1 : "Tata consultancy services",
+        title2 : "System Engineer",
+        location : "Hyderabad",
+        descrption : "Worked on a browser-based application using ReactJS, Collaborated with cross-functional teams for on-time milestones. Implemented optimized REST APIs for seamless backend connectivity.Monitored key metrics, enhancing overall user experience. Contributed to project success through efficient development",
+        date : "Apr 2021 - Jan 2023",
+        icon : "work",
+    },
+    {
+        id : 3,
+        title1 : "Gossner College	Ranchi",
+        title2 : "Bachelor of Computer Application",
+        location : "Ranchi",
+        descrption : "79.88 %",
+        date : "June 2017 – Nov 2020",
+        icon : "school",
+    },
+    {
+        id : 4,
+        title1 : "Usha Martin Ltd",
+        title2 : "Administrative Intern",
+        location : "Ranchi",
+        descrption : "Assisted in the development and implementation of new administrative procedures on the SAP system. Applied advanced Excel and SQL techniques for data analysis and reporting",
+        date : "Dec 2019 – Feb 2020",
+        icon : "work",
+    },
+    {
+        id : 5,
+        title1 : "DAV Public School",
+        title2 : "Senior Secondary",
+        location : "Ranchi",
+        descrption : "60 %",
+        date : "Jul 2015 – May 2017",
+        icon : "school",
+    }
+]
